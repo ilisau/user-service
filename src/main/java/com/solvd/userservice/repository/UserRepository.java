c1865b07e8c5eb3d6c434b21a272c669fbe35517
ilisau <ilisov.laba@solvd.com>
2023-03-22 16:02:19 +0300

    Fix after review

--- src/main/java/com/solvd/userservice/repository/UserRepository.java
+++ src/main/java/com/solvd/userservice/repository/UserRepository.java
@@ -2,10 +2,8 @@ package com.solvd.userservice.repository;
 
 import com.solvd.userservice.domain.User;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
 
-@Repository
 public interface UserRepository extends ReactiveMongoRepository<User, String> {
 
     Mono<User> findByEmail(String email);
