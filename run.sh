8ce75bd0e0a1d337ba661d1e0f14511b33fde857
ilisau <ilisov.laba@solvd.com>
2023-03-24 12:06:29 +0300

    Implement K8s configs for Redis

--- run.sh
+++ run.sh
@@ -1,11 +1,14 @@
 cd src/infra || exit
 kubectl apply -f mongo-secrets.yml
 kubectl apply -f user-client-configmap.yml
+kubectl apply -f redis-configmap.yml
 
 kubectl apply -f user-client-service.yml
 kubectl apply -f mongo-service.yml
+kubectl apply -f redis-service.yml
 
 kubectl apply -f user-client-deployment.yml
 kubectl apply -f mongo-stateful.yml
+kubectl apply -f redis-stateful.yml
 
 minikube service user-client
