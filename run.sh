90aa3479b044ce67d6935ef40486c9311c5454ce
Ilya Lisov <ilya.lisov.yt@gmail.com>
2023-04-11 13:29:07 +0300

    Fix Liquibase and Kubernetes

--- run.sh
+++ run.sh
@@ -2,13 +2,16 @@ cd src/infra || exit
 kubectl apply -f mongo-secrets.yml
 kubectl apply -f user-client-configmap.yml
 kubectl apply -f redis-configmap.yml
+kubectl apply -f pg-kafka-secrets.yml
 
 kubectl apply -f user-client-service.yml
 kubectl apply -f mongo-service.yml
 kubectl apply -f redis-service.yml
+kubectl apply -f pg-kafka-service.yml
 
 kubectl apply -f user-client-deployment.yml
 kubectl apply -f mongo-stateful.yml
 kubectl apply -f redis-stateful.yml
+kubectl apply -f pg-kafka-stateful.yml
 
 minikube service user-client
