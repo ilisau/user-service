0b832108469161f95331a622550aec51a2c00d3a
Ilya Lisov <ilisov@solvd.com>
2023-06-05 11:30:26 +0300

    #31 Implement GraphQL

--- src/main/java/com/solvd/userservice/web/controller/UserController.java
+++ src/main/java/com/solvd/userservice/web/controller/UserController.java
@@ -12,6 +12,9 @@ import com.solvd.userservice.web.dto.validation.OnUpdate;
 import com.solvd.userservice.web.mapper.PasswordMapper;
 import com.solvd.userservice.web.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -38,30 +41,32 @@ public class UserController {
     /**
      * Create user.
      *
-     * @param userDto user dto
+     * @param user user dto
      * @return empty response
      */
     @PostMapping
+    @MutationMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Mono<Void> create(
-            @Validated(OnCreate.class) @RequestBody final UserDto userDto
+    public Mono<Void> createUser(
+            @Validated(OnCreate.class) @RequestBody @Argument final UserDto user
     ) {
-        User user = userMapper.toEntity(userDto);
-        return userEventService.create(user);
+        User u = userMapper.toEntity(user);
+        return userEventService.create(u);
     }
 
     /**
      * Update user.
      *
-     * @param userDto user dto
+     * @param user user dto
      * @return empty response
      */
     @PutMapping
-    public Mono<Void> update(
-            @Validated(OnUpdate.class) @RequestBody final UserDto userDto
+    @MutationMapping
+    public Mono<Void> updateUser(
+            @Validated(OnUpdate.class) @RequestBody @Argument final UserDto user
     ) {
-        User user = userMapper.toEntity(userDto);
-        return userEventService.update(user);
+        User u = userMapper.toEntity(user);
+        return userEventService.update(u);
     }
 
     /**
@@ -71,7 +76,8 @@ public class UserController {
      * @return user dto
      */
     @GetMapping("/{id}")
-    public Mono<UserDto> getById(@PathVariable final String id) {
+    @QueryMapping
+    public Mono<UserDto> userById(@PathVariable @Argument final String id) {
         Mono<User> user = userQueryService.getById(id);
         return user.map(userMapper::toDto);
     }
@@ -79,16 +85,18 @@ public class UserController {
     /**
      * Change user password.
      *
-     * @param id          user id
-     * @param passwordDto password dto
+     * @param id       user id
+     * @param password password dto
      * @return empty response
      */
     @PutMapping("/{id}/password")
-    public Mono<Void> changePassword(@PathVariable final String id,
-                                     @RequestBody final PasswordDto passwordDto
+    @MutationMapping
+    public Mono<Void> changePassword(@Argument @PathVariable final String id,
+                                     @Argument
+                                     @RequestBody final PasswordDto password
     ) {
-        Password password = passwordMapper.toEntity(passwordDto);
-        return userEventService.updatePassword(id, password);
+        Password p = passwordMapper.toEntity(password);
+        return userEventService.updatePassword(id, p);
     }
 
     /**
@@ -99,8 +107,11 @@ public class UserController {
      * @return empty response
      */
     @PostMapping("/{id}/password")
-    public Mono<Void> setPassword(@PathVariable final String id,
-                                  @RequestBody final String newPassword) {
+    @MutationMapping
+    public Mono<Void> setPassword(@Argument @PathVariable final String id,
+                                  @Argument
+                                  @RequestBody final String newPassword
+    ) {
         return userEventService.updatePassword(id, newPassword);
     }
 
@@ -111,7 +122,9 @@ public class UserController {
      * @return user dto
      */
     @GetMapping("/email/{email}")
-    public Mono<UserDto> getByEmail(@PathVariable final String email) {
+    @QueryMapping
+    public Mono<UserDto> userByEmail(@PathVariable @Argument final String email
+    ) {
         Mono<User> user = userQueryService.getByEmail(email);
         return user.map(userMapper::toDto);
     }
@@ -119,12 +132,14 @@ public class UserController {
     /**
      * Activate user.
      *
-     * @param jwtToken jwt token
+     * @param token jwt token
      * @return empty response
      */
     @PostMapping("/activate")
-    public Mono<Void> activate(@RequestBody final JwtToken jwtToken) {
-        return userEventService.activate(jwtToken);
+    @MutationMapping
+    public Mono<Void> activateUser(@RequestBody @Argument final JwtToken token
+    ) {
+        return userEventService.activate(token);
     }
 
     /**
@@ -134,8 +149,9 @@ public class UserController {
      * @return empty response
      */
     @DeleteMapping("/{id}")
+    @MutationMapping
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public Mono<Void> deleteById(@PathVariable final String id) {
+    public Mono<Void> deleteUser(@PathVariable @Argument final String id) {
         return userEventService.delete(id);
     }
 
